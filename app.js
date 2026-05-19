41060e96e668fdf8204904bb5f6c69579152bfe0
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-05-19 10:30:28 +0800

    Add IP checking endpoint and trust proxy setting

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ import express from 'express'
 
 
 var app = express();
-
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
+app.get('/check-ip', (req, res) => {
+  // req.ip now contains the user's real IP address
+  const userIP = req.ip;
+  res.json({ userIP });
+});
 app.post('/', (req, res) => {
     const userIP = req.headers['x-forwarded-for'];
     if(!userIP){
